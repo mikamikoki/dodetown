68691cc5bcef7a3adb9351099422e53232e1acfb
mikamikoki <angjisanshang@gmail.com>
2022-01-07 03:09:06 +0000

    gem,controller,routingの追加

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,8 @@
+class PostImagesController < ApplicationController
+
+  def index
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  root to: 'post_images#index'
+  resources :post_images, only: [:new, :create, :edit, :show, :destroy, :update] do
+  resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update, :index]
+  resource :favorites, only: [:create, :destroy]
+
 end
