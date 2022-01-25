170eec39e23cace1f6e86b0060f4d79894e47162
mikamikoki <angjisanshang@gmail.com>
2022-01-25 05:24:11 +0000

    ヘッダーアイコン追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -40,8 +40,6 @@ class PostImagesController < ApplicationController
 
   def update
    @post_image = PostImage.find(params[:id])
-   p post_image_params
-
    if @post_image.update(post_image_params)
     flash[:notice] = "編集しました！"
    redirect_to post_image_path(@post_image.id)
